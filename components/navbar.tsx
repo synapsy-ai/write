b418f5b44f5acc4f8c9db889b5849fad3984a5c8
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-10-01 20:03:14 +0200

    Fixed z-index issues

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 export default function NavBar(props: { lng: string }) {
   const { t } = useTranslation(props.lng, "common");
   return (
-    <div className="flex items-center justify-center sticky top-0">
-      <nav className="flex items-center space-x-2 dark:bg-slate-950/80 bg-white/80 backdrop-blur-lg shadow-md px-4 py-2 m-2 rounded-full">
+    <div className="flex items-center justify-center sticky top-0 z-50">
+      <nav className="flex items-center space-x-2 dark:bg-slate-950/80 bg-white/80 backdrop-blur-md shadow-md px-4 py-2 m-2 rounded-full">
         <Image alt="RativeGenLogo" height={25} width={25} src={"/logo.svg"} />
         <h2 className="font-bold">{t("title")}</h2>
         <Link href={"/" + props.lng}>
