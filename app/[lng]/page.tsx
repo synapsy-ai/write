3b3dd0ac85e68d544820b4833d74eb2f878e86db
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-10-01 11:18:00 +0200

    Added home page content

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,12 +1,27 @@
 "use client";
+import Image from "next/image";
 import { useTranslation } from "../i18n/client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home({ params: { lng } }: { params: { lng: any } }) {
   const { t } = useTranslation(lng, "common");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>{t("title")}</p>
+    <main>
+      <section className="flex flex-col min-h-screen justify-center items-center p-4 text-center">
+        <Image
+          alt="The logo of Quark Write"
+          src="logo.svg"
+          width={256}
+          height={256}
+        />
+        <h2 className="font-bold text-3xl mt-8">{t("introducing-quark")}</h2>
+        <p>{t("introducing-quark-text")}</p>
+        <Link href={lng + "/create"}>
+          <Button className="m-2">{t("launch")}</Button>
+        </Link>
+      </section>
     </main>
   );
 }
