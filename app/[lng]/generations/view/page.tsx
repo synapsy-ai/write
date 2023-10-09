4d15d6e60cee20473fbe0d86e679cb61eb50c564
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-10-09 17:47:09 +0200

    Added stats section

--- app/[lng]/generations/view/page.tsx
+++ app/[lng]/generations/view/page.tsx
@@ -3,6 +3,7 @@
 import ResultDisplayer from "@/components/result-displayer";
 import { HistoryItem } from "@/lib/history";
 import { useSearchParams } from "next/navigation";
+import { encode } from "gpt-token-utils";
 
 export default function GenerationViewPage({
   params,
@@ -20,6 +21,37 @@ export default function GenerationViewPage({
   if (typeof window !== "undefined") {
     el = JSON.parse(localStorage.getItem("synapsy_write_history") ?? "[]")[id];
   }
+
+  function getPrice() {
+    try {
+      if (typeof window !== "undefined") {
+        let e = encode(
+          JSON.parse(localStorage.getItem("synapsy_write_history") ?? "[]")[id]
+            .content,
+        );
+        let price = `\$${((e.length / 1000) * 0.06).toFixed(4)}`;
+        return price.toString();
+      }
+      return "$0";
+    } catch (error) {
+      return "$0";
+    }
+  }
+  function countWords() {
+    if (typeof window !== "undefined") {
+      return JSON.parse(localStorage.getItem("synapsy_write_history") ?? "[]")[
+        id
+      ].content.split(" ").length;
+    }
+  }
+
+  function countChars() {
+    if (typeof window !== "undefined") {
+      return JSON.parse(localStorage.getItem("synapsy_write_history") ?? "[]")[
+        id
+      ].content.length;
+    }
+  }
   return (
     <main>
       <section className="flex flex-col items-center justify-center">
@@ -28,6 +60,22 @@ export default function GenerationViewPage({
           id="ct"
         >
           <ResultDisplayer res={el.content} type={el.template} />
+        </section>
+        <section className="m-2 flex flex-wrap items-center justify-center print:hidden">
+          <div className="m-2 w-48 rounded-lg bg-white p-4 shadow-md dark:bg-slate-900">
+            <h2 className="font-bold">Prix</h2>
+            <p id="price" className="text-2xl font-bold">
+              {getPrice()}
+            </p>
+          </div>
+          <div className="m-2 w-48 rounded-lg bg-white p-4 shadow-md dark:bg-slate-900">
+            <h2 className="font-bold">Mots</h2>
+            <p className="text-2xl font-bold">{countWords()}</p>
+          </div>
+          <div className="m-2 w-48 rounded-lg bg-white p-4 shadow-md dark:bg-slate-900">
+            <h2 className="font-bold">Caractères</h2>
+            <p className="text-2xl font-bold">{countChars()}</p>
+          </div>
         </section>
       </section>
     </main>
