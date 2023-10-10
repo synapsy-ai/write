d299f192f9b79db991c6bb394595ab570299ebe9
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-10-10 19:44:49 +0200

    Added export and import buttons

--- app/[lng]/generations/page.tsx
+++ app/[lng]/generations/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { useTranslation } from "@/app/i18n/client";
 import { GenerationItem } from "@/components/generation-item";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { HistoryItem } from "@/lib/history";
-import { Eraser } from "lucide-react";
+import { Download, Eraser, Upload } from "lucide-react";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Creations({
@@ -18,12 +21,55 @@ export default function Creations({
   }
 
   const [noItems, setNoItems] = useState(history.length == 0);
-
+  function Import(event: any) {
+    let file = event.target.files[0]; // get the selected file
+    let reader = new FileReader(); // create a FileReader object
+    reader.onload = function (event) {
+      let text: string = event.target?.result as string; // get the file content as text
+      localStorage.setItem("synapsy_write_history", text);
+    };
+    reader.readAsText(file); // read the file as text
+  }
   return (
     <main className="m-2">
       <header>
         <h2 className="text-2xl font-bold">{t("generations")}</h2>
         <p>{t("no-gen-text")}</p>
+        <Input
+          type="file"
+          id="FileSelector"
+          accept="application/json"
+          className="hidden"
+          onChange={Import}
+        ></Input>
+        <div className="flex">
+          <Link
+            href={
+              "data:text/plain;charset=UTF-8," +
+              encodeURIComponent(
+                typeof window !== "undefined"
+                  ? JSON.stringify(history)
+                  : "{msg: 'an error occurred'}",
+              )
+            }
+            download={"generations.json"}
+          >
+            <Button variant="link" className="space-x-2">
+              <Upload height={16} /> <p>{t("export")}</p>
+            </Button>
+          </Link>
+          <Button
+            variant="link"
+            onClick={() =>
+              (
+                document.getElementById("FileSelector") as HTMLInputElement
+              ).click()
+            }
+            className="space-x-2"
+          >
+            <Download height={16} /> <p>{t("import")}</p>
+          </Button>
+        </div>
       </header>
       {!noItems ? (
         <section className="flex flex-wrap justify-center p-5 md:justify-start">
