5eff0f5e2c6643e9962a95ec57c84b77617a24e5
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-10-15 17:41:27 +0200

    Added meta tags

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -28,6 +28,28 @@ export default function RootLayout({
     <html lang={lng} dir={dir(lng)}>
       <head>
         <link rel="icon" href="/logo.png" />
+
+        <meta
+          name="twitter:description"
+          content="A new way to write text, powered by AI."
+        />
+        <meta name="twitter:site" content="@PeyronnetGroup" />
+        <meta name="twitter:title" content="Synapsy Write" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:image"
+          content="https://peyronnet.group/synapsy_write_social.png"
+        />
+
+        <meta name="og:title" content="Synapsy Write" />
+        <meta
+          name="og:description"
+          content="A new way to write text, powered by AI."
+        />
+        <meta
+          name="og:image"
+          content="https://peyronnet.group/synapsy_write_social.png"
+        />
         <Script
           id="gtag"
           async
