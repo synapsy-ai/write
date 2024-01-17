8df1868ef5d4c05c55260949c9bc616005623acf
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-01-17 19:34:37 +0100

    Added new settings button in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,13 +3,7 @@ import { useTranslation } from "@/app/i18n/client";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
-import { Sun, Moon } from "lucide-react";
+import { Settings } from "lucide-react";
 import { useTheme } from "next-themes";
 
 export default function NavBar(props: { lng: string }) {
@@ -22,32 +16,17 @@ export default function NavBar(props: { lng: string }) {
           <Image alt="SynapsyLogo" height={25} width={25} src={"/logo.svg"} />
           <h2 className="font-bold">{t("title")}</h2>
         </Link>
-        <Link href={"/" + props.lng + "/create"}>
+        <Link href={"/create"}>
           <Button variant="ghost">{t("create")}</Button>
         </Link>
-        <Link href={"/" + props.lng + "/generations"}>
+        <Link href={"/generations"}>
           <Button variant="ghost">{t("generations")}</Button>
         </Link>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon">
-              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span className="sr-only">Toggle theme</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme("light")}>
-              {t("light")}
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>
-              {t("dark")}
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("system")}>
-              {t("system")}
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Link href={"/settings"} className="group">
+          <Button variant="ghost" size="icon">
+            <Settings className="h-[1.2rem] w-[1.2rem] transition-all group-hover:rotate-90" />
+          </Button>
+        </Link>
       </nav>
     </div>
   );
