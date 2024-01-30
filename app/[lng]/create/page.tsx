a73d8fa042a7a78dc078833c68beac3cf48c3afd
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-01-30 17:00:39 +0100

    Subscription is now required to access Synapsy Write

--- app/[lng]/create/page.tsx
+++ app/[lng]/create/page.tsx
@@ -1,17 +1,53 @@
-import { redirect } from "next/navigation";
 import Create from "./create";
-import { getSession } from "@/app/supabase-server";
+import {
+  getActiveProductsWithPrices,
+  getSession,
+  getSubscriptions,
+} from "@/app/supabase-server";
 import NoSession from "./no-session";
+import BuySubscription from "./buy-subscription";
 
 export default async function CreatePage({
   params: { lng },
 }: {
   params: { lng: any };
 }) {
-  const [session] = await Promise.all([getSession()]);
-
+  const [session, products, subscriptions] = await Promise.all([
+    getSession(),
+    getActiveProductsWithPrices(),
+    getSubscriptions(),
+  ]);
   if (!session) {
     return <NoSession lng={lng} />;
   }
+
+  if (!subscriptions || subscriptions.length < 1) {
+    return (
+      <BuySubscription
+        session={session}
+        products={products}
+        subscriptions={subscriptions}
+        lng={lng}
+      />
+    );
+  }
+  for (let i = 0; i < subscriptions?.length; i++) {
+    if (
+      !subscriptions[i].prices?.products?.name
+        ?.toString()
+        .toLowerCase()
+        .includes("write")
+    ) {
+      return (
+        <BuySubscription
+          session={session}
+          products={products}
+          subscriptions={subscriptions}
+          lng={lng}
+        />
+      );
+    }
+  }
+
   return <Create lng={lng} />;
 }
