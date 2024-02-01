c0eb3382c5b1e078db9bb225a17be17444405ebf
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-02-01 15:27:06 +0100

    Added spotlight component

---
+++ components/spotlight.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import React, { useRef, useState, useEffect } from "react";
+import MousePosition from "@/utils/mouse-position";
+
+type SpotlightProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function Spotlight({
+  children,
+  className = "",
+}: SpotlightProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const mousePosition = MousePosition();
+  const mouse = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const containerSize = useRef<{ w: number; h: number }>({ w: 0, h: 0 });
+  const [boxes, setBoxes] = useState<Array<HTMLElement>>([]);
+
+  useEffect(() => {
+    containerRef.current &&
+      setBoxes(
+        Array.from(containerRef.current.children).map(
+          (el) => el as HTMLElement,
+        ),
+      );
+  }, []);
+
+  useEffect(() => {
+    initContainer();
+    window.addEventListener("resize", initContainer);
+
+    return () => {
+      window.removeEventListener("resize", initContainer);
+    };
+  }, [setBoxes]);
+
+  useEffect(() => {
+    onMouseMove();
+  }, [mousePosition]);
+
+  const initContainer = () => {
+    if (containerRef.current) {
+      containerSize.current.w = containerRef.current.offsetWidth;
+      containerSize.current.h = containerRef.current.offsetHeight;
+    }
+  };
+
+  const onMouseMove = () => {
+    if (containerRef.current) {
+      const rect = containerRef.current.getBoundingClientRect();
+      const { w, h } = containerSize.current;
+      const x = mousePosition.x - rect.left;
+      const y = mousePosition.y - rect.top;
+      const inside = x < w && x > 0 && y < h && y > 0;
+      if (inside) {
+        mouse.current.x = x;
+        mouse.current.y = y;
+        boxes.forEach((box) => {
+          const boxX =
+            -(box.getBoundingClientRect().left - rect.left) + mouse.current.x;
+          const boxY =
+            -(box.getBoundingClientRect().top - rect.top) + mouse.current.y;
+          box.style.setProperty("--mouse-x", `${boxX}px`);
+          box.style.setProperty("--mouse-y", `${boxY}px`);
+        });
+      }
+    }
+  };
+
+  return (
+    <div className={className} ref={containerRef}>
+      {children}
+    </div>
+  );
+}
+
+type SpotlightCardProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export function SpotlightCard({
+  children,
+  className = "",
+}: SpotlightCardProps) {
+  return (
+    <div
+      className={`relative overflow-hidden rounded-lg bg-white p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-slate-400 before:opacity-0 before:blur-[100px] before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-96 after:w-96 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-[100px] after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100 dark:bg-slate-900 ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
