1677330c6da1f237b0bee77816b8a78493c4d4b3
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-03-26 17:55:07 +0100

    Added quota system

--- app/[lng]/create/page.tsx
+++ app/[lng]/create/page.tsx
@@ -3,6 +3,8 @@ import {
   getActiveProductsWithPrices,
   getSession,
   getSubscriptions,
+  getUserDetails,
+  setUserQuotas,
 } from "@/app/supabase-server";
 import NoSession from "./no-session";
 import BuySubscription from "./buy-subscription";
@@ -18,7 +20,45 @@ export default async function CreatePage({
     getActiveProductsWithPrices(),
     getSubscriptions(),
   ]);
-
+  async function getQuotas() {
+    if (!session || !session.user) return 0;
+    const user = await getUserDetails();
+    if (!user) return 0;
+    if (!user?.write_gpt4_quota) {
+      if (isSubscribed()) {
+        const q = getInterval() == "year" ? 120 : 10;
+        setUserQuotas(user.id, q);
+        return q;
+      }
+    }
+    return 0;
+  }
+  function getInterval(): "month" | "year" | "none" {
+    if (!session || !subscriptions) return "none";
+    for (let i = 0; i < subscriptions?.length; i++) {
+      if (
+        subscriptions[i].prices?.products?.name?.toLowerCase().includes("write")
+      ) {
+        const period =
+          +subscriptions[i].current_period_end -
+          parseInt(subscriptions[i].current_period_start);
+        return period > 2764800 ? "year" : "month";
+      }
+    }
+    return "none";
+  }
+  function isSubscribed(): boolean {
+    if (!session || !subscriptions) return false;
+    for (let i = 0; i < subscriptions?.length; i++) {
+      if (
+        subscriptions[i].prices?.products?.name?.toLowerCase().includes("write")
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+  getQuotas();
   return (
     <Create
       session={session}
