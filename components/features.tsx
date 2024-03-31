38ed3bd502ac71bf6e59cae29b464f7d1adcc461
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-03-31 17:20:38 +0200

    Updated pricing table

--- components/features.tsx
+++ components/features.tsx
@@ -11,6 +11,50 @@ export default function PricingFeatures(props: Props) {
   if (!props.productName.toLowerCase().includes("synapsy write")) {
     return <></>;
   }
+  if (props.productName.includes("premium")) {
+    return (
+      <div className="mt-8 grid grid-cols-[auto,1fr] items-center gap-x-2  dark:text-slate-300">
+        <Check size={16} />
+        <p>
+          {t("feature-ai")} (20/{t("month")})
+        </p>
+        <Check size={16} />
+        <p>{t("advanced-instructions")}</p>
+        <Check size={16} />
+        <p>{t("table-generator")}</p>
+        <Check size={16} />
+        <p>{t("essays")}</p>
+        <Check size={16} />
+        <p>{t("text-analysis")}</p>
+        <Check size={16} />
+        <p>{t("variable-editor")}</p>
+      </div>
+    );
+  }
+
+  if (props.productName.includes("pro")) {
+    return (
+      <div className="mt-8 grid grid-cols-[auto,1fr] items-center gap-x-2  dark:text-slate-300">
+        <Check size={16} />
+        <p>
+          {t("feature-ai")} ({t("unlimited")})
+        </p>
+        <Check size={16} />
+        <p>{t("unlimited-access")}</p>
+        <Check size={16} />
+        <p>{t("advanced-instructions")}</p>
+        <Check size={16} />
+        <p>{t("table-generator")}</p>
+        <Check size={16} />
+        <p>{t("essays")}</p>
+        <Check size={16} />
+        <p>{t("text-analysis")}</p>
+        <Check size={16} />
+        <p>{t("variable-editor")}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="mt-8 grid grid-cols-[auto,1fr] items-center gap-x-2  dark:text-slate-300">
       <Check size={16} />
