dedb799b06a0187c956f9ffcdd5e4e9b925c27f4
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-07-21 13:30:23 +0200

    Added Template system

--- lib/recipe.ts
+++ lib/recipe.ts
@@ -11,3 +11,42 @@ export type Recipe = {
   systemPrompt: string;
   steps: Step[];
 };
+
+export function addToTemplates(item: Recipe) {
+  if (typeof window !== "undefined") {
+    let templates: Recipe[] = [];
+    templates = JSON.parse(
+      localStorage.getItem("synapsy_write_templates") ?? "[]",
+    );
+    templates.push(item);
+    localStorage.setItem("synapsy_write_templates", JSON.stringify(templates));
+  }
+}
+
+export function removeFromTemplates(index: number) {
+  if (typeof window !== "undefined") {
+    let templates: Recipe[] = [];
+    templates = JSON.parse(
+      localStorage.getItem("synapsy_write_templates") ?? "[]",
+    );
+    templates.splice(index, 1);
+    localStorage.setItem("synapsy_write_templates", JSON.stringify(templates));
+  }
+}
+
+export function getTemplates(): Recipe[] {
+  if (typeof window !== "undefined") {
+    let templates: Recipe[] = [];
+    templates = JSON.parse(
+      localStorage.getItem("synapsy_write_templates") ?? "[]",
+    );
+    return templates;
+  }
+  return [];
+}
+
+export function saveTemplates(recipes: Recipe[]) {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("synapsy_write_templates", JSON.stringify(recipes));
+  }
+}
