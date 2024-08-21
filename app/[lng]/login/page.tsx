7a09258ff800fdffb08a9e5b6dc0e24a3b87e64d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-08-21 12:34:41 +0200

    Added new Sign In page

--- app/[lng]/login/page.tsx
+++ app/[lng]/login/page.tsx
@@ -1,21 +1,5 @@
-import LoginPage from "./auth";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-export default async function SignIn({
-  params: { lng },
-}: {
-  params: { lng: any };
-}) {
-  const cookieStore = cookies();
-  const supabase = createServerComponentClient({ cookies: () => cookieStore });
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (user) {
-    return redirect("/me");
-  }
-  return <LoginPage lng={lng} />;
+export default function SignIn() {
+  return redirect(`/signin/password_signin`);
 }
