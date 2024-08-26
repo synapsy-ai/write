91b7c61470003581ec189fcd2448626f65783fab
Léo Peyronnet <leo.peyronnet@outlook.fr>
2024-08-26 20:41:33 +0200

    Added Spanish language

--- app/[lng]/settings/page.tsx
+++ app/[lng]/settings/page.tsx
@@ -218,6 +218,7 @@ export default function SettingsPage({
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="en">English</SelectItem>
+                  <SelectItem value="es">Español</SelectItem>
                   <SelectItem value="fr">Français</SelectItem>
                 </SelectContent>
               </Select>
