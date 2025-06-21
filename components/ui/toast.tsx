da3c4467d85b9673f269b7aa21e29eaf23627370
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-06-21 18:54:02 +0200

    Upgraded to RadixUI package

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import * as ToastPrimitives from "@radix-ui/react-toast"
+import { Toast as ToastPrimitives } from "radix-ui"
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
 
