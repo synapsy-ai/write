464cef43ee35782fab458b0d2af9f8ceba20aa9e
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-07-31 17:48:42 +0200

    Fixed null border issue

--- components/ui/moving-border.tsx
+++ components/ui/moving-border.tsx
@@ -79,7 +79,7 @@ export const MovingBorder = ({
   ry?: string;
   [key: string]: any;
 }) => {
-  const pathRef = useRef<any>();
+  const pathRef = useRef<any>(null);
   const progress = useMotionValue<number>(0);
 
   useAnimationFrame((time) => {
