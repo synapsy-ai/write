fc01cbfab33b008a856df3f25b9b9f6462359b0d
Léo Peyronnet <leo.peyronnet@outlook.fr>
2025-09-02 10:39:34 +0200

    Updated Stripe

--- utils/stripe/config.ts
+++ utils/stripe/config.ts
@@ -6,7 +6,7 @@ export const stripe = new Stripe(
   {
     // https://github.com/stripe/stripe-node#configuration
     // https://stripe.com/docs/api/versioning
-    apiVersion: "2025-07-30.basil",
+    apiVersion: "2025-08-27.basil",
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
